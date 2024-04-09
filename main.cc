4e39e90338cf31d6b8ef9035dba89f1f037d85aa
piotr <piotr@beesoft.pl>
2024-04-09 15:39:13 +0200

    open or create database

--- main.cc
+++ main.cc
@@ -5,46 +5,28 @@
 #include <fmt/core.h>
 #include <string>
 
-int main(int argc, char *argv[]) {
-    auto const ok = SQLite::instance().create("/home/piotr/notes.sqlite", [](SQLite const& db){
-        using namespace std::string_literals;
-        if (db.exec(Category::Create)) {
-            if (db.exec(Note::Create)) {
-                if (db.insert("INSERT INTO category (pid, name) VALUES (?,?)", 1959, "Piotrek"s) == -1)
-                    return false;
-                if (db.insert("INSERT INTO category (pid, name) VALUES (?,?)", 1987, "Artur & Błażej"s) == -1)
-                    return false;
-                if (db.insert("INSERT INTO note (pid, title, content) VALUES(?, ?, ?)", 2043, "tuuł", "notatka") == -1)
-                    return false;
-            }
-        }
-        return true;
-    }, true);
 
-    if (not ok) {
-        fmt::print("ERROR\n");
+bool open_or_create_database() noexcept {
+    using namespace std::string_literals;
+    static auto const DatabasePath = "/home/piotr/notes.sqlite"s;
+
+    // Try to open.
+    if (SQLite::instance().open(DatabasePath))
+        return true;
+
+    // Can't open so create one.
+    return SQLite::instance().create(DatabasePath, [](SQLite const& db){
+        // Create tables.
+        return db.exec(Category::Create) and db.exec(Note::Create);
+    });
+}
+
+
+int main(int argc, char *argv[]) {
+    if (not open_or_create_database()) {
+        fmt::print(stderr, "Database creation error\n");
         return 1;
     }
-
-    {
-
-        if (SQLite::instance().update("UPDATE category SET pid=?, name=? WHERE id=?", 2099, "Yolanda", 2))
-            fmt::print("updated\n");
-
-        if (auto result = SQLite::instance().select("SELECT * FROM category"); result) {
-            auto data = result.value();
-            for (auto it = data.begin(); it != data.end(); ++it)
-                fmt::print("{}\n", it->description());
-        }
-    }
-
-
-
-//    SQLite::instance().create(SQLite::InMemory, [](SQLite const& db){
-//        if (not db.exec(Category::Create)) return false;
-//        return true;
-//    });
-
 
 //    QApplication a(argc, argv);
 //    QPushButton button("Hello world!", nullptr);
